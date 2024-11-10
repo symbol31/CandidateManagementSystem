c12d6982b32944fe10fa6050649d62992e4ca6e1
Pratik <symbolsapkota@gmail.com>
2024-11-10 13:45:22 +0545

    feat(api): controller added for api

---
+++ WebApi/Controllers/CandidateController.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.CandidateManagement.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Dtos;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CandidateController : ControllerBase
+    {
+        private readonly ICandidateService _candidateService;
+
+        public CandidateController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrUpdateCandidate([FromBody] UpsertCandidateDto dto)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            Candidate candidate = new Candidate
+            {
+                Email = dto.Email,
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                PhoneNumber = dto.PhoneNumber,
+                PreferredCallStartTime = dto.PreferredCallStartTime,
+                PreferredCallEndTime = dto.PreferredCallEndTime,
+                LinkedInProfile = dto.LinkedInProfile,
+                GitHubProfile = dto.GitHubProfile,
+                Comments = dto.Comments
+            };
+            await _candidateService.UpsertCandidateAsync(candidate);
+            return Ok(new { Message = "Candidate processed successfully" });
+        }
+    }
+}
+
---
+++ WebApi/Dtos/UpsertCandidateDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApi.Dtos
+{
+    public class UpsertCandidateDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public TimeSpan? PreferredCallStartTime { get; set; }
+        public TimeSpan? PreferredCallEndTime { get; set; }
+        public string? LinkedInProfile { get; set; }
+        public string? GitHubProfile { get; set; }
+        [Required]
+        public string Comments { get; set; }
+    }
+}
