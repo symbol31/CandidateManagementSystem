14097dafeeba406bc9ec6e187786b202d7cc7609
Pratik <symbolsapkota@gmail.com>
2024-11-10 13:42:01 +0545

    feat(initialSetup): Project setup done alongwith the DbContext.

---
+++ Infrastructure/Persistence/CandidateDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class CandidateDbContext : DbContext
+    {
+        public DbSet<Candidate> Candidates { get; set; }
+        public CandidateDbContext(DbContextOptions<CandidateDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Candidate>()
+                .HasIndex(c => c.Email)
+                .IsUnique();
+
+        }
+    }
+}
